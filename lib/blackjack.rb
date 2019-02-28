056767754170e6d5a009a9dc535ba6d9b32315a8
delightful-tree-6762 <mduckworth@gmail.com>
2019-02-28 05:57:20 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -59,7 +59,7 @@ end
 #####################################################
 
 def runner
-  welcome 
+  welcome
   card_total = initial_round
   until card_total > 21
     card_total = hit?(card_total)

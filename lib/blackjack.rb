a27c724c02b8eec5e491a600d37b2ba64e90ed08
delightful-tree-6762 <mduckworth@gmail.com>
2019-02-28 05:37:03 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -14,14 +14,17 @@ def display_card_total
 end
 
 def prompt_user
+  puts "Type "h" to hit or "s" to stay"
   # code #prompt_user here
 end
 
 def get_user_input
+  gets "user input"
   # code #get_user_input here
 end
 
 def end_game
+  puts "Sorry, you hit #{card_total}. Thanks for playing"
   # code #end_game here
 end
 
